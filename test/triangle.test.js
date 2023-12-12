e4aa150edbd5dde77af8d6b3845e3ba991795425
Roopa Thimmanacherla <roopareddy90@gmail.com>
2023-12-12 09:06:47 -0800

    added test files

--- test/triangle.test.js
+++ test/triangle.test.js
@@ -1,14 +1,10 @@
 const triangle = require("../lib/triangle.js");
-describe("circle", () => {
+const triangleExample = require("./triangleExample.js");
+describe("Triangle", () => {
   describe("display triangle logo", () => {
     it("should render triangle filled  with color in the browser", () => {
       const shape = new triangle("svg", "white", "blue");
-      expect(shape.render()).toEqual(`<svg version="1.1" 
-      width="300" height="200"
-      xmlns="http://www.w3.org/2000/svg">
-      <polygon points="100, 15 200, 200 0, 200" fill="blue"/>
-      <text x="100" y="185" font-size="70" text-anchor="middle" fill="white">SVG</text>
-  </svg>`);
+      expect(shape.render()).toMatch(triangleExample);
     });
   });
 });
--- test/triangleExample.js
+++ test/triangleExample.js
@@ -1,6 +1,7 @@
-<svg version="1.1" 
+const triangleExample = `<svg version="1.1" 
     width="300" height="200"
     xmlns="http://www.w3.org/2000/svg">
     <polygon points="100, 15 200, 200 0, 200" fill="blue"/>
     <text x="100" y="185" font-size="70" text-anchor="middle" fill="white">svg</text>
-</svg>
+</svg>`;
+module.exports = triangleExample;
